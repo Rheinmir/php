919580a737e9f7fb8dc9370bc8e7977456fc1b0f
Rheinmir <rheinmir@gmail.com>
2024-03-15 05:52:35 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$_rhein = 'rheinmir';
+$bien_so_1 = 12;
+
+// đây là commment 1 dòng
+
+/*comment nhiều dòng*/
+
+$bienSoMot = 12.5;
+
+var_dump($_rhein);
+
+// echo 'rhein_PHP <br>';
+// echo $_rhein;
+// echo 100;
+
+$arr = [1,2,3,4];
+
+print_r($arr);
