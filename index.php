9920f87085030ce7f56551a4b0e67bd1eb452562
Rheinmir <rheinmir@gmail.com>
2024-03-16 04:40:13 +0700

    nested if else

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
+//nested if else
     $var = 2;
-    if($var > 10){ //if bool = true, then execute codes in scope, if bool not true, then execute code in else's scope
-        echo "var bigger than 10";
-    }
-    else if($var > 5){ //if bool = true, then execute codes in scope, if bool not true, then execute code in else's scope
-        echo "var1 bigger than 5";
-    }
-    else if($var > 3){ //if bool = true, then execute codes in scope, if bool not true, then execute code in else's scope
-        echo "var bigger than 3";
-    }
-    else {
-        echo "var does not meet any conditions";
+    if($var > 0){ //if bool = true, then execute codes in scope
+        if($var > 3){ //if bool = true, then execute codes in scope, if bool not true, then execute code in else's scope
+            echo "var bigger than 3";
+        }
+        else {
+            echo "var does not bigger than 3";
+        }
     }
 ?>
