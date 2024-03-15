88aff9c5f7202e0605af74d13e35f5f19e7cd5ab
Rheinmir <rheinmir@gmail.com>
2024-03-15 07:48:12 +0700

    constrant

--- index.php
+++ index.php
@@ -1,35 +1,11 @@
 <?php
-$bien_so_1 = "rheinmir";
-$bien_so_2 = 'Nintendo' . $bien_so_1.'is learning PHP';
-$bienTong = "Nintendo $bien_so_1 is learning PHP";
+define('RHEIN_WEB_HOST',10);
 
-$bien1= 'một';
-$bien2= 'hai';
-$bien3= 'ba';
-$bien4= 'bốn';
-
-echo '<ul>
-<li>1 '.$bien1.'</li>
-<li>2 '.$bien2.'</li>
-<li>3 '.$bien3.'</li>
-<li>4 '.$bien4.'</li>
-</ul>';
-
-
-
-// đây là commment 1 dòng
-
-/*comment nhiều dòng*/
-
-// $bienSoMot = 12.5;
-
-// var_dump($_rhein);
-
-// echo 'rhein_PHP <br>';
-// echo $_rhein;
-// echo 100;
-
-// $arr = [1,2,3,4];
-
-// print_r($arr);
+echo RHEIN_WEB_HOST;
+/*name constant
+- including letters, number, underscores(_)
+- starting with letter or underscore
+- case sensitive
+- following underscore rules with uppercase(eg: HOST_ROOT) or using underscore like the first letter is recommended
+*/
 ?>
