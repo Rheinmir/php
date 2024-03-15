c6b02ded67ac92f492b28418c78190a38b7bc7e7
Rheinmir <rheinmir@gmail.com>
2024-03-15 07:27:27 +0700

    variable

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <?php
-$_rhein = 'rheinmir';
-$bien_so_1 = 12;
+$bien_so_1 = "rheinmir";
+$bien_so_2 = 'Nintendo' . $bien_so_1.'is learning PHP';
+$bienTong = "Nintendo $bien_so_1 is learning PHP";
+
+$bien1= 'một';
+$bien2= 'hai';
+$bien3= 'ba';
+$bien4= 'bốn';
+
+echo '<ul>
+<li>1 '.$bien1.'</li>
+<li>2 '.$bien2.'</li>
+<li>3 '.$bien3.'</li>
+<li>4 '.$bien4.'</li>
+</ul>';
+
+
 
 // đây là commment 1 dòng
 
 /*comment nhiều dòng*/
 
-$bienSoMot = 12.5;
+// $bienSoMot = 12.5;
 
-var_dump($_rhein);
+// var_dump($_rhein);
 
 // echo 'rhein_PHP <br>';
 // echo $_rhein;
 // echo 100;
 
-$arr = [1,2,3,4];
+// $arr = [1,2,3,4];
 
-print_r($arr);
+// print_r($arr);
+?>
