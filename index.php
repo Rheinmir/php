c95026efaf5b0c7c75165f9a88e28c8f0f9c9427
Rheinmir <rheinmir@gmail.com>
2024-03-16 05:00:25 +0700

    for loop

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
-//switch case
-$day = 8;
-switch ($day) {
-    case 2:
-        echo "Today is Mon";
-        break;
-    case 3:
-        echo "Today is Tue";
-        break;
-    case 4:
-        echo "Today is Wed";
-        break;
-    case 5:
-        echo "Today is Thu";
-        break;
-    case 6:
-        echo "Today is Fri";
-        break;
-    case 7:
-        echo "Today is Sat";
-        break;
-    default:
-        echo "Today is Sun";
-        break;
+//for
+for($i =0; $i < 10; $i++){
+    echo $i. '<br>';
 }
