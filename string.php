3f5b22896daf7d132bd06152c9298b4b7b76a2f9
Rheinmir <rheinmir@gmail.com>
2024-03-16 14:42:58 +0700

    string execute function

--- string.php
+++ string.php
@@ -1,13 +1,21 @@
 <?php
-// $var1 = 'rhein';
-// $var2 = "rhein";
-
-// define('_AGENCY', 'rhein');
-// define('_AGENCY2', "rhein");
-$string1 = "learn | programing | with | PHP";
+//explode
+$string1 = "learn | programming | with | PHP";
 $arr = explode('|', $string1);
 
 var_dump($string1);
 echo '<br>';
 var_dump($arr);
+
+print_r($arr);
+//implode
+$string2 = implode('|',$arr);
+echo $string2.'<br>';
+
+//strlen
+$string3 = 'learn programming with PHP';
+echo strlen($string3).'<br>';
+//str_word_count
+echo str_word_count($string3);
 ?>
+
