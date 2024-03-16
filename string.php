16754e0276bc55476462660c066b3754c7a00156
Rheinmir <rheinmir@gmail.com>
2024-03-16 14:20:37 +0700

    string

--- string.php
+++ string.php
@@ -4,6 +4,9 @@
 
 // define('_AGENCY', 'rhein');
 // define('_AGENCY2', "rhein");
+$Mir = 'nnn';
+echo "Rhein\"Mir\"- learning PHP".'<br>';
+echo "Rhein$Mir - learning PHP".'<br>';
+echo 'Rhein"Mir"- learning PHP'.'<br>';
 
-echo "Rhein\"Mir\"- learning PHP";
 ?>
