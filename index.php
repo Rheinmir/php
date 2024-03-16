f8b12fac5fb381755a9308253dd24c5fd32a574b
Rheinmir <rheinmir@gmail.com>
2024-03-16 09:28:33 +0700

    odd and even detect loop

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
 <?php
-//for
-for($i =0; $i < 10; $i++){
-    echo $i. '<br>';
+//display even or odd (using for loop)
+$start = 1;
+$end = 100;
+
+$oddCount = 0;
+$evenCount = 0;
+
+$oddResult = null;
+$evenResult = null;
+
+for($i = $start;$i <=100; $i++)
+{
+    //tcheck odd or even
+    if($i % 2 == 0){
+        $evenResult .= $i .'  ';
+        $evenCount++;
+    }
+    else {
+        $oddResult .= $i .'  ';
+        $oddCount++;
+    }
 }
-echo '<br><br><br>';
-for($i =0; $i <= 10; $i += 2){
-    echo $i. '<br>';
+
+if($evenCount > 0){
+    echo "Find out" .$evenCount." even number: ".$evenResult;
+}
+else {
+    echo "Cant find evens";
+}
+
+if($oddCount > 0){
+    echo "Find out" .$oddCount." odd number: ".$oddResult;
+}
+else {
+    echo "Cant find odds";
 }
