d3af9a992d7f99fd99e4aa886ce5f3b5f5d7c890
Rheinmir <rheinmir@gmail.com>
2024-03-16 13:59:11 +0700

    string define

---
+++ string.php
@@ -0,0 +1,19 @@
+<?php
+$var1 = 'rhein';
+$var2 = "rhein";
+
+define('_AGENCY', 'rhein');
+define('_AGENCY2', "rhein");
+
+var_dump($var1);
+echo '<br>';
+
+var_dump($var2);
+echo '<br>';
+
+var_dump(_AGENCY);
+echo '<br>';
+
+var_dump(_AGENCY2);
+echo '<br>';
+?>
