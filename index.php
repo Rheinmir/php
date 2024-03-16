95a9ad9530b542522be1bdf9d2b3ac1015c3873b
Rheinmir <rheinmir@gmail.com>
2024-03-16 09:56:36 +0700

    factorial loop

--- index.php
+++ index.php
@@ -1,37 +1,14 @@
 <?php
-//display even or odd (using for loop)
-$start = 1;
-$end = 100;
-
-$oddCount = 0;
-$evenCount = 0;
-
-$oddResult = null;
-$evenResult = null;
-
-for($i = $start;$i <=100; $i++)
-{
-    //tcheck odd or even
-    if($i % 2 == 0){
-        $evenResult .= $i .'  ';
-        $evenCount++;
+//factorial loop
+$n = 10;
+if ($n > 0) {
+    $result = 1;
+    for ($i = 1; $i <= $n; $i++) {
+        $result *= $i;
     }
-    else {
-        $oddResult .= $i .'  ';
-        $oddCount++;
-    }
+    echo "result = ".$result.'<br>';
+}
+else{
+    echo "the input is not valid";
 }
 
-if($evenCount > 0){
-    echo "Find out " .$evenCount." even number: ".$evenResult;
-}
-else {
-    echo "Cant find evens";
-}
-echo '<br><br>';
-if($oddCount > 0){
-    echo "Find out " .$oddCount." odd number: ".$oddResult;
-}
-else {
-    echo "Cant find odds";
-}
