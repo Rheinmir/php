3a012ff790964621d60b6e948d029b58514cde92
Rheinmir <rheinmir@gmail.com>
2024-03-16 14:06:37 +0700

    string escape \" \"

--- string.php
+++ string.php
@@ -1,19 +1,9 @@
 <?php
-$var1 = 'rhein';
-$var2 = "rhein";
+// $var1 = 'rhein';
+// $var2 = "rhein";
 
-define('_AGENCY', 'rhein');
-define('_AGENCY2', "rhein");
+// define('_AGENCY', 'rhein');
+// define('_AGENCY2', "rhein");
 
-var_dump($var1);
-echo '<br>';
-
-var_dump($var2);
-echo '<br>';
-
-var_dump(_AGENCY);
-echo '<br>';
-
-var_dump(_AGENCY2);
-echo '<br>';
+echo "Rhein\"Mir\"- learning PHP";
 ?>
