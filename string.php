dd242dacb265de29824015f58823105510c5458e
Rheinmir <rheinmir@gmail.com>
2024-03-17 07:38:16 +0700

    execute string application

--- string.php
+++ string.php
@@ -1,70 +1,36 @@
 <?php
-//explode
-$string1 = "learn | programming | with | PHP";
-$arr = explode('|', $string1);
+//take 5 characters in end of the string
+$string = "learn programming php";
+$endStr = substr($string, -5);
+echo '5 character in the end of the string is: ' .$endStr.'<br>';
 
-var_dump($string1);
-echo '<br>';
-var_dump($arr);
+$string2 = "Học lập trình php";
+$endStr2 = mb_substr($string2, -9, null, 'UTF-8'); //mb_substr($string,$position, $length,'encoding') execute special character
+echo '9 character in the end of the string is: ' .$endStr2.'<br>';
 
-print_r($arr);
-//implode
-$string2 = implode('|',$arr);
-echo $string2.'<br>';
+//Delete the first character in the string
+$strName = 'Rhein Mir';
+$posSpace = strpos($strName,' ');// find posistion of the first space
 
-//strlen
-$string3 = 'learn programming with PHP';
-echo strlen($string3).'<br>';
-//str_word_count
-echo str_word_count($string3).'<br>';
-//str_repeat
-echo str_repeat($string3,3).'<br>';
-//str_replace
-$string4 = "learning - program - with - php";
-echo str_replace('-','|',$string4).'<br>';
-//md5 encode
-$string5 = 'learnphp';
-echo md5($string5).'<br>';
-//sha1 encode
-echo sha1($string5).'<br>';
-// htmlentities
-echo htmlentities('<p>php</p>');
-// html_entity_decode
-$str = htmlentities('<b>php</b>');
-echo '</br>html_entity_decode: '.html_entity_decode($str).'<br>';
-//strip_tags
-echo strip_tags('<b>php</b>');
-//substr($string,$start,$length)
-$string7 = "learn programming php <br>";
-$string8 = "using MySQL";
-$string9 = "MySQL";
-$newString7 = substr($string7, 6, 11);
-echo '<br>' .$newString7. '<br>';
-//strstr($string,$start_str) //input starting characters and this func'll cut string from there
-echo strstr($string7,'pr');
-//strpos($string, $substr)
-$strpos = strpos($string7, 'programming' );
-echo $strpos. '<br>';
-//substr_replace($string $replace,$position,$length)
-echo substr_replace($string7, $string8, 6); // if not input length it'll replace all string after inputed position
+$posStr = $posSpace + 1; //started position of the word after first space
 
+//Length of string want to cut
+$endWordLen = strlen($strName) - $posStr;
 
-//strtolower($str)// turn all string to lowercase
-echo '<br>'.strtolower($string8);
-//strtoipper($str)// turn all string to uppercase
-echo '<br>'.strtoupper($string8);
-//ucfirst
-echo '<br>'.ucfirst($string8);
-//lcfirst
-echo '<br>'.lcfirst($string9);
-//ucwords($str) first character of words in the string turn to uppercase
-echo '<br>'.ucwords($string7);
-//trim($str, $char) delete first and final character, if not input $char, it means space character.
-echo '<br>'.trim('Enicode','e'); // distinguish uppercase and lowercase letters
-//json_encode; json_decode
-$arr= [1,2,3,4,5];
-$jsonencode = json_encode($arr);
-echo $jsonencode.'<br>';
+//Cut string
+$endWord = substr($strName, $posStr, $endWordLen);
+echo '<br>'. $endWord.'<br>';
 
-$jsondecode = json_decode($jsonencode,true);
-print_r($jsondecode);
+//Delete the first character in the string
+$strName2 = 'Trần Hoàng Gia';
+$posSpace2 = mb_strpos($strName2,' ',0, 'UTF-8');// mb_strpos($string,$strFindPos,$offset,'UTF-8') find posistion of the first space
+
+$posStr2 = $posSpace2 + 1; //started position of the word after first space
+
+//Length of string want to cut
+$endWordLen2 = mb_strlen($strName2, 'UTF-8') - $posStr2;
+
+//Cut string
+$endWord2 = mb_substr($strName2, $posStr2, $endWordLen2);
+echo '<br>'.$endWord2.'<br>';
+?>
