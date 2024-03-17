8a337d17fa60e83e523bc1ef8722448291e30b7f
Rheinmir <rheinmir@gmail.com>
2024-03-18 06:43:30 +0700

    multidimensional array

--- array.php
+++ array.php
@@ -1,43 +1,71 @@
- <?php
-    //declare array
-    $arr = array('HTML','JS','PHP');
-    $arr2 = ['address1' => 'HTML','address2' => 'JS','address3' => 'PHP'];
+ <?php   
+    
 
-    //Add element to array
-    $arr2['address4'] = 'NodeJS';
-    $arr2[] = 'C';
-    $arr2[] = 'C++';
-    array_push($arr,'NodeJs','C','C++','C#');
+    // echo $arr2['address1']['name']['lastName'].'<br>';
+    // echo $arr2['address1']['email'].'<br>';
 
-  
+    // $arr2['address1']['name']['firstName'] = 'Unux';
+    // echo $arr2['address1']['name']['firstName'].'<br>';
 
-    // //read array
-    // $rhein = $arr2['address4'];
-    // echo $rhein.'<br>';
+    $arr = array(
+        [
+            'first',
+            'second',
+            'third'
+        ],
+        'JS',
+        'PHP'
+    );
 
-    //using for loop
-    // if(!empty($arr)){
-    //     for($i=0; $i < count($arr); $i++){
-    //         echo $arr[$i] .'<br>';
+        if(!empty($arr)){
+            for($i = 0;$i < count($arr);$i++){
+                if(is_array($arr[$i])){
+                    if(!empty($arr[$i])){
+                        for($j=0; $j < count($arr[$i]); $j++){
+                            echo $arr[$i][$j]."<br>";
+                        }
+                    }
+                }
+                echo $arr[$i].'<br>';
+            }
+        };
 
-    //     }
-    // }
-
-    //using foreach with non-consecutive indexes in array
-    if(!empty($arr2)){
-        foreach($arr2 as $key => $value){
-            echo $value .'<br>';
+        $arr2 = [
+            'address1' => [
+                'name' => [
+                    'firstName' => 'Jin',
+                    'lastName' => 'Air'
+                ],
+                'email' => 'jinAir@gmail.com'
+            ],
+            'address2' => [
+                'name' => 'Henry',
+                'email' => 'henry@gmail.com'
+            ],
+            'address3' => 'PHP'
+        ];
+        
+        if(!empty($arr2)){
+            foreach($arr2 as $item){
+                if (is_array($item)){
+                    if(!empty($item)){
+                        foreach($item as $subArr){
+                            if(is_array($subArr)){
+                                if(!empty($subArr)){
+                                    foreach($subArr as $subArr2){
+                                        echo $subArr2.'<br>';
+                                    }
+                                }
+                            }
+                            else {
+                                echo $subArr. "<br>";
+                            }
+                        }
+                    }
+                }
+                else {
+                    echo $item. "<br>";
+                }
+            }
         }
-    }
-    //replace array
-
-    $arr[0] = 'HTMLNEW';
-    $arr[2] = 'Python';
-    //delete array
-    unset($arr2['address1']);
-
-      //print array
-      echo '<pre>';
-      print_r($arr2);
-      echo '</pre>';
 ?>
