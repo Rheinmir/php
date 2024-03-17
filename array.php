5f9d1573291fcfbde3c882b1109f1e8ce99e4991
Rheinmir <rheinmir@gmail.com>
2024-03-18 04:43:21 +0700

    array

--- array.php
+++ array.php
@@ -9,13 +9,35 @@
     $arr2[] = 'C++';
     array_push($arr,'NodeJs','C','C++','C#');
 
-    //print array
-    echo '<pre>';
-    print_r($arr);
-    echo '</pre>';
+  
 
-    //read array
-    $rhein = $arr2['address4'];
-    echo $rhein;
+    // //read array
+    // $rhein = $arr2['address4'];
+    // echo $rhein.'<br>';
 
+    //using for loop
+    // if(!empty($arr)){
+    //     for($i=0; $i < count($arr); $i++){
+    //         echo $arr[$i] .'<br>';
+
+    //     }
+    // }
+
+    //using foreach with non-consecutive indexes in array
+    if(!empty($arr2)){
+        foreach($arr2 as $key => $value){
+            echo $value .'<br>';
+        }
+    }
+    //replace array
+
+    $arr[0] = 'HTMLNEW';
+    $arr[2] = 'Python';
+    //delete array
+    unset($arr2['address1']);
+
+      //print array
+      echo '<pre>';
+      print_r($arr2);
+      echo '</pre>';
 ?>
